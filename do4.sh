a4f95d674443b43fb6574057bed454ba3dfc0c82
Andrea <massironi.andrea@gmail.com>
2014-11-14 15:23:55 +0100

    update instructions

---
+++ do4.sh
@@ -0,0 +1,16 @@
+
+cd /home/amassiro/Latinos/CMSSW_5_3_11_patch6/src/HWWAnalysis/ShapeAnalysis/
+eval `scramv1 runtime -sh`
+source test/env.sh
+cd -
+
+
+gardener.py  hwidthMVAVar -k 1  /home/amassiro/Latinos/Shape/tree_skim_all_hwidth/nominals/latino_169_qqww1smTM_baseW.root.temp.root /home/amassiro/Latinos/Shape/tree_skim_all_hwidth/nominals/latino_169_qqww1smTM_baseW.root
+gardener.py  hwidthMVAVar -k 1 /home/amassiro/Latinos/Shape/tree_skim_all_hwidth/nominals/latino_170_qqww9smTT_baseW.root.temp.root /home/amassiro/Latinos/Shape/tree_skim_all_hwidth/nominals/latino_170_qqww9smTT_baseW.root
+gardener.py  hwidthMVAVar -k 1 /home/amassiro/Latinos/Shape/tree_skim_all_hwidth/nominals/latino_171_qqww25smTM_baseW.root.temp.root /home/amassiro/Latinos/Shape/tree_skim_all_hwidth/nominals/latino_171_qqww25smTM_baseW.root
+gardener.py  hwidthMVAVar -k 1 /home/amassiro/Latinos/Shape/tree_skim_all_hwidth/nominals/latino_172_qqww1smTE_baseW.root.temp.root /home/amassiro/Latinos/Shape/tree_skim_all_hwidth/nominals/latino_172_qqww1smTE_baseW.root
+gardener.py  hwidthMVAVar -k 1 /home/amassiro/Latinos/Shape/tree_skim_all_hwidth/nominals/latino_173_qqww9smTE_baseW.root.temp.root /home/amassiro/Latinos/Shape/tree_skim_all_hwidth/nominals/latino_173_qqww9smTE_baseW.root
+gardener.py  hwidthMVAVar -k 1 /home/amassiro/Latinos/Shape/tree_skim_all_hwidth/nominals/latino_174_qqww25smTE_baseW.root.temp.root /home/amassiro/Latinos/Shape/tree_skim_all_hwidth/nominals/latino_174_qqww25smTE_baseW.root
+gardener.py  hwidthMVAVar -k 1 /home/amassiro/Latinos/Shape/tree_skim_all_hwidth/nominals/latino_175_qqww9smTM_baseW.root.temp.root /home/amassiro/Latinos/Shape/tree_skim_all_hwidth/nominals/latino_175_qqww9smTM_baseW.root
+gardener.py  hwidthMVAVar -k 1 /home/amassiro/Latinos/Shape/tree_skim_all_hwidth/nominals/latino_176_qqww1smTT_baseW.root.temp.root /home/amassiro/Latinos/Shape/tree_skim_all_hwidth/nominals/latino_176_qqww1smTT_baseW.root
+gardener.py  hwidthMVAVar -k 1 /home/amassiro/Latinos/Shape/tree_skim_all_hwidth/nominals/latino_177_qqww25smTT_baseW.root.temp.root /home/amassiro/Latinos/Shape/tree_skim_all_hwidth/nominals/latino_177_qqww25smTT_baseW.root
